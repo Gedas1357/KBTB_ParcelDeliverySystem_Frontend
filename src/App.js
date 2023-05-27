29bc03c8ec750b022dec18fd4022df84fbd6c44f
Gedas1357 <gedasvilnietis@gmail.com>
2023-05-27 20:11:39 +0300

    Added a customer page and made the button only seen by admin

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Route, Routes} from 'react-router-dom';
 import LoginPage from "./components/LoginPage";
 import EditPage from "./components/EditPage";
 import ParcelForm from "./components/ParcelForm";
-import CustomerTable from "./components/CustomerTable";
+import CustomerPage from "./components/CustomerPage";
 import ParcelTable from "./components/ParcelTable";
 import NavBar from "./components/NavBar";
 import PaymentPage from "./components/PaymentPage";
@@ -15,20 +15,20 @@ function App() {
 
   return (<Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/edit-info" element={<><NavBar /><EditPage /> </>} />
-            <Route path="/payment" element={<PaymentPage />} />
+            <Route path="/edit-info" element={<><NavBar /><EditPage /></>} />
+            <Route path="/payment" element={<><NavBar /><PaymentPage /></>} />
+            <Route path="/customers" element={<><NavBar /><CustomerPage /></>} />
             <Route path="/homepage" element={
                 <>
                     <NavBar />
                     <div className="w-full p-4 h-screen flex flex-row justify-evenly">
                         <div className='basis-1/2 h-5/6 flex flex-col justify-evenly'>
-                            <h1>Customers</h1>
-                            <CustomerTable/>
-                            <h1>Parcels</h1>
+
+                            <h1 className="self-center">Parcels</h1>
                             <ParcelTable/>
                         </div>
                         <div className='basis-1/6 h-5/6 flex flex-col justify-evenly'>
-                            <h1>Add a new parcel</h1>
+                            <h1 className="self-center">Add a new parcel</h1>
                             <ParcelForm/>
                         </div>
                     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import {Link, useNavigate} from "react-router-dom";
 
 const NavBar = () => {
     const navigate = useNavigate();
+    const username = localStorage.getItem('username');
     const parcelNav = () => {
         navigate('/homepage');
     };
@@ -13,11 +14,15 @@ const NavBar = () => {
         localStorage.clear();
         navigate('/');
     };
+    const customerNav = () => {
+        navigate('/customers');
+    };
 
     return (
         <div className="flex flex-row w-screen h-14 justify-evenly items-center border-2 border-black">
             <button className='basis-2/12 h-5/6' onClick={parcelNav}>Parcels</button>
             <button className='basis-2/12 h-5/6' onClick={editNav}>Edit profile</button>
+            {username == 'admin' && <button className='basis-2/12 h-5/6' onClick={customerNav}>Customers</button>}
             <button className='basis-2/12 h-5/6' onClick={logOut}>Log Out</button>
         </div>
     )
