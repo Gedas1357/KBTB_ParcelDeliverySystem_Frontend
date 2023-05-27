05bb5ae70ec4e4ac45960ee3d5e777130b50a5ba
Gedas1357 <gedasvilnietis@gmail.com>
2023-05-27 19:42:43 +0300

    Deleted unused code

--- src/App.js
+++ src/App.js
@@ -28,8 +28,6 @@ function App() {
                             <ParcelTable/>
                         </div>
                         <div className='basis-1/6 h-5/6 flex flex-col justify-evenly'>
-                            <h1>Add a new customer</h1>
-                            
                             <h1>Add a new parcel</h1>
                             <ParcelForm/>
                         </div>
