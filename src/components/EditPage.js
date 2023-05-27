c21f82bb9a931cc2de66d69ba8e61a455536da62
Gedas1357 <gedasvilnietis@gmail.com>
2023-05-27 21:00:18 +0300

    Redid EditPage a bit

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { checkSessionExpiration } from '../requests/Authentication';
+//import "./../App.css";
 
 const UserInfoForm = () => {
   const [username, setUsername] = useState('');
@@ -13,101 +14,61 @@ const UserInfoForm = () => {
 
 
   useEffect(() => {
-   
-    const fetchUserInfo = async () => { 
-
-        const authHeader = JSON.parse(localStorage.getItem('authHeader'));
-
-        try {
-            console.log('checky');
-            checkSessionExpiration();
-            console.log('big success here is yor aydy sir:');
-
-        } catch (error) {
-        console.error('session over bye:', error);
-        
-        }
-        
-        //console.log(localStorage.getItem('username'));
-        try {
-            const name = localStorage.getItem('username');
-            console.log(name);
-            const response = await axios.get(`http://localhost:8080/api/V1/customer/name/${name}`, { headers: authHeader }); 
-            console.log(response);
-            const id  = response.data;
-            setId(id);
-            console.log(id);
-
-            if (authHeader ) {
-                        console.log('Authorization Header:', authHeader);
-                        try {
-                            console.log('aidi',id);
-                            const response = await axios.get('http://localhost:8080/api/V1/customer/' + id, { headers: authHeader }); 
-                            console.log(response);
-                            const { username } = response.data;
-                            const { email } = response.data;
-                            const { address } = response.data;
-                            const {version} = response.data;
-                            setUsername(username);
-                            setEmail(email);
-                            setAddress(address);
-                            setVersion(version);
-                    } catch (error) {
-                    console.error('Error fetching user info:', error);
-                }
-                    } else {
-                        console.log('User not authenticated');
-                    }
-
-        } catch (error) {
-        console.error('err:', error);
-        
-        }   
-    };
-
-    fetchUserInfo();
+      const fetchUserInfo = async () => {
+          const authHeader = JSON.parse(localStorage.getItem('authHeader'));
+          try {
+              const name = localStorage.getItem('username');
+              const response = await axios.get(`http://localhost:8080/api/V1/customer/name/${name}`, { headers: authHeader });
+              const id  = response.data;
+              setId(id);
+              try {
+                  const response = await axios.get('http://localhost:8080/api/V1/customer/' + id, { headers: authHeader });
+                  const { username } = response.data;
+                  const { email } = response.data;
+                  const { address } = response.data;
+                  const {version} = response.data;
+                  setUsername(username);
+                  setEmail(email);
+                  setAddress(address);
+                  setVersion(version);
+              } catch (error) {
+                  console.error('Error fetching user info: ', error);
+              }
+          } catch (error) {
+              console.error('err: ', error);
+          }
+      };
+      fetchUserInfo();
   }, []);
 
   const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    const authHeader = JSON.parse(localStorage.getItem('authHeader'));
+      event.preventDefault();
+      const authHeader = JSON.parse(localStorage.getItem('authHeader'));
 
-    try {
-        console.log(authHeader );
-      await axios.put(`http://localhost:8080/api/V1/customer/${id}`, {
-        username,
-        email,
-        address,
-        version
-      },  { headers: authHeader });
-
-     // const response = await axios.get(`http://localhost:8080/api/V1/customer/${id}`);
-      
-    //  setUsername(response.data.username);
-      //setEmail(response.data.email);
-      //setId(response.data.id);
-      window.location.reload();
-      console.log('User information updated successfully');
-      
-    } catch (error) {
-        if (error.response && error.response.status === 409) {
-            setErrorMessage('Conflict occurred while updating user, choose one of the following actions:');
-            setShowActionButtons(true); 
-      //console.error('Error updating user information:', error);
-    }
-    else {
-        console.error('Error updating user information:', error);
+      try {
+          console.log(authHeader);
+          await axios.put(`http://localhost:8080/api/V1/customer/${id}`, {
+              username,
+              email,
+              address,
+              version
+          },  { headers: authHeader });
+          window.location.reload();
+      } catch (error) {
+          if (error.response && error.response.status === 409) {
+              setErrorMessage('Conflict occurred while updating user, choose one of the following actions:');
+              setShowActionButtons(true);
+          } else {
+              console.error('Error updating user information:', error);
+          }
       }
-    }
   };
 
-  //cia viskas ok
   const handleReload = () => {
     window.location.reload();
   };
 
 
-  //dar nzn ar veiks 
   const handleBlindOverwrite = async(event) => {
     event.preventDefault();
     const authHeader = JSON.parse(localStorage.getItem('authHeader'));
@@ -116,64 +77,54 @@ const UserInfoForm = () => {
         const response = await axios.get(`http://localhost:8080/api/V1/customer/${id}`, { headers: authHeader });
         const version = response.data.version;
         try {
-            //console.log('ver:', version);
             await axios.put(`http://localhost:8080/api/V1/customer/${id}`, {
                 username,
                 email,
                 address,
                 version
             },  { headers: authHeader });
-
             window.location.reload();
-            console.log('User information updated successfully');
-        
         } catch (error) {
             if (error.response && error.response.status === 409) {
                 setErrorMessage('Conflict occurred while updating user, choose one of the following actions:');
-                setShowActionButtons(true); 
-        //console.error('Error updating user information:', error);
+                setShowActionButtons(true);
+            }
+            else {
+                console.error('Error updating user information:', error);
+            }
         }
-        else {
-            console.error('Error updating user information:', error);
-        }
-        }
-      } catch (error) {
+    } catch (error) {
         console.error('Error fetching user information:', error);
-      }
+    }
   };
 
   return (
-    <div>
-    <h2>Welcome, {username}!</h2>
-    {errorMessage && <p>{errorMessage}</p>}
-      {showActionButtons && (
-        <div>
-         <button onClick={() => handleReload()}>Reload page to see new information and try again</button>
-          <button  onClick={handleBlindOverwrite}>Try again with same information</button>
-        </div>
-      )}
-    <form onSubmit={handleFormSubmit}>
-      <label>
-        Email:
-        <input
-          type="text"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Address:
-        <input
-          type="text"
-          value={address}
-          onChange={(event) => setAddress(event.target.value)}
-        />
-      </label>
-      <br />
-      <button type="submit">Update</button>
-    </form>
-  </div>
+      <div className='flex flex-col justify-evenly h-96'>
+          <h1 className='self-center'>Welcome, {username}!</h1>
+          {errorMessage && <p className='self-center text-red-600 font-bold'>{errorMessage}</p>}
+          {showActionButtons && (
+          <div className='flex flex-col basis-1/5 self-center justify-evenly'>
+              <button className='w-full self-center' onClick={() => handleReload()}>Reload page to see new information and try again</button>
+              <button className='w-full self-center' onClick={handleBlindOverwrite}>Try again with same information</button>
+          </div>)}
+          <form onSubmit={handleFormSubmit} className='basis-1/5 w-1/5 self-center'>
+              <label> Email:</label>
+              <input
+                  type="text"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+              />
+              <br />
+              <label>Address:</label>
+              <input
+                  type="text"
+                  value={address}
+                  onChange={(event) => setAddress(event.target.value)}
+              />
+              <br />
+              <button type="submit">Update</button>
+          </form>
+      </div>
   );
 };
 
